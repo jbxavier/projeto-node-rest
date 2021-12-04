0a9f59f5700f783012b1dc8b1bd5203dc3204385
Jean Bueno Xavier <jbxavier@gmail.com>
2021-12-04 16:54:15 -0300

    configuração do post concluído

--- config/customExpress.js
+++ config/customExpress.js
@@ -6,8 +6,13 @@ const express = require('express')
 // agrupa app atendimentos para ser reconhecido 
 const consign = require('consign')
 
+const bodyParser = require('body-parser')
+
 module.exports = () => {
     const app = express()
+    // adicionando bibliotecas do parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
     // usar consign: colocar pasta de controllers para ser visto pelo
     // app (estamos passando app para ser visto pelos arquivos de 
     // controllers)
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,9 @@
 
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos!'))
+    
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimento e está realizando um Post')
+    })
 }
